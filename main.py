cfbc4c81d584a862ad8778afddc30252fc6ea5c4
iadanielz <138160092+iadanielz@users.noreply.github.com>
2023-12-01 18:36:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.gmaps import Gmaps
 star_it = '''Love It? Star It! ⭐ https://github.com/omkarcloud/google-maps-scraper/'''
 
-queries = ["web developers in bangalore"]
-Gmaps.places(queries, max=5)
+queries = ["coffee shops in san francisco"]
+Gmaps.places(queries, max=100, min_reviews=5, max_reviews=100, has_phone=True, has_website=False, sort=[Gmaps.SORT_BY_REVIEWS_DESCENDING, Gmaps.SORT_BY_NOT_HAS_WEBSITE])
